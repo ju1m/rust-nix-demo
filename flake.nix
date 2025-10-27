d0e86993d6daf0ee09512089e2489e3cc963cfe4
Julien Moutinho <julm+rust@sourcephile.fr>
2025-10-27 20:56:51 +0100

    maint(nix): remove unused variable

--- flake.nix
+++ flake.nix
@@ -40,12 +40,6 @@
   outputs =
     inputs:
     let
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
       lib = inputs.nixpkgs.lib;
       foreachSystem =
         doWithArgs:
